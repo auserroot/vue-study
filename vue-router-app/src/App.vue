c64ec67b31f10afe4e20e33eeac8730715086500
auserroot <f1998fzy@outlook.com>
2021-01-20 10:08:55 +0800

    add

---
+++ vue-router-app/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <!-- 声明式路由 -->
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <!-- 编程式路由 -->
+      <button @click="toother">other</button>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    // 编程式路由
+    toother(){
+      this.$router.history.push({
+        path:'/other'
+      })
+    }
+  },
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ vue-router-app/src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path:'/other',
+    name:'other',
+    component:()=>import('../components/other.vue')
+  },
+  {
+    path: '/404',
+    name: 'notfoundpage',
+    component:() => import('../components/notfoundpage.vue')
+  },
+  {
+    path:'*',
+    redirect:'/404'
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
